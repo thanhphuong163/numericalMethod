4f9511e1b8b77dad9e9b043dc0b0e9338c92b4b6
Nguyen Thanh Phuong <thanhphuong.its@gmail.com>
2017-06-20 22:51:44 +0700

    Bla bla

--- solver_equation_simpexLoop.m
+++ solver_equation_simpexLoop.m
@@ -1,15 +1,37 @@
 clc;
-% Giai phuong trinh phi tuyen bang phuong phap lap don
-% Cac tham so nhap vao gom ham f(x), khoang cach ly nghiem va do chinh xac
+% Giai phuong trinh phi tuyen bang phuong phap lap don.
+% Cac tham so nhap vao gom ham g(x), khoang cach ly nghiem, do chinh xac
+% va gia tri khoi tao.
+% Truoc tien, kiem tra su hoi tu cua ham. Neu hoi tu thi tiep tuc giai, con
+% ko thi dung va thong bao ra man hinh.
 syms x
-func = input('Nhap ham so f(x) = ');
-f = inline(func);
+func = input('Nhap ham so g(x) = ');
+g = inline(func);
 disp('Nhap khoang cach ly nghiem [a,b]:');
 a = input('a = ');
 b = input('b = ');
 accuracy = input('Nhap do chinh xac mong muon:');
 
+dg = diff(func);
+dg = inline(dg);
+if dg((a+b)/2) < 0
+    q = dg(a);
+else
+    q = dg(b);
+end
 
+if abs(q) >= 1
+    disp('Ham g(x) khong hoi tu.');
+else
+    x0 = input('Nhap gia tri khoi dau x0 = ');
+    dx = abs(g(x0) - x0);
+    while dx > accuracy
+        x1 = g(x0);
+        dx = abs(g(x0) - x1);
+        x0 = x1;
+    end
+    fprintf('Phuong trinh co nghiem trong khoang cach ly nghiem: x = %d\n',x0);
+end
 
 
 
